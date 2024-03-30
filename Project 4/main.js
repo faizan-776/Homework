0af190598d86dcdb09e778c2163bc33478b4720d
faizan-776 <164095645+faizan-776@users.noreply.github.com>
2024-03-30 16:06:48 -0700

    Add files via upload

--- Project 4/main.js
+++ Project 4/main.js
@@ -1,3 +1,1 @@
-var message = '"Peoples live dont end when they die. It ends when they lose faith."';
-famous_person = "Itachi Uchiha";
-console.log(famous_person + ' once said,' + message);
+console.log('Miyamoto Musashi once said, ' + '"Think lightly of yourself and deeply of the world."');
