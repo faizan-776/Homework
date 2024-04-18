e606e8f8ece5876665a530d4702b3c32a041e3ef
faizan-776 <164095645+faizan-776@users.noreply.github.com>
2024-04-18 11:44:38 +0500

    Delete project 8 directory

--- project 8/main.ts
+++
@@ -1,5 +0,0 @@
-//This program includes the name of the author and his one quote
-console.log('Miyamoto Musashi once said, ' + '"Think lightly of yourself and deeply of the world."');
-//The program below is based on the favourite color and the name of the person
-let favNumber: number = 7
-console.log("My favourite number is " + favNumber)
